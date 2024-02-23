54608fec50ae1534c7579fe31831db20e94ffc97
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2024-02-23 14:25:55 +0100

    Added hardcoded login

--- Main.py
+++ Main.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)  
+
+USERNAME = 'admin'
+PASSWORD = 'password'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == USERNAME and password == PASSWORD:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid Credentials. Please try again.'
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('index.html')  
 
 if __name__ == '__main__':
     app.run(debug=True)
