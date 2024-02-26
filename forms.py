119bbc0a09a82a45d59bed0e02eaf6ced462cbc5
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2024-02-26 12:38:38 +0100

    Email validator geimporteerd in main.py

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, EmailField, SubmitField
 from wtforms.validators import DataRequired, Email
+import email_validator
 
 class InputForm(FlaskForm):
     user_input = StringField('Enter something:', validators=[DataRequired()])
